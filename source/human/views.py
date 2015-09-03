f0227edf94577e1eb398bf247eaa13f9fbef0d30
rakeshsukla53 <rakesh.sukla53@gmail.com>
2015-09-03 00:04:12 -0400

    form setup in view

--- source/human/views.py
+++ source/human/views.py
@@ -7,6 +7,9 @@ def human(request):
     #context is some sort of objects or variables that we want to bring into our project
     title = "Welcome"
     form = UserForm()  #creating instance of the form
+
+    if request.method == 'POST':
+        print request.POST #if you want to see the data  #if you want to view the DATA
     if request.user.is_authenticated():   #this is the user authentication method here
 
         title = "Human Trafficking" + " " + str(request.user)
